917bbcac52426c23c9a0b4ecd31ff9fa283089a2
Robin Hooda <robinhooda66@gmail.com>
2020-04-10 11:07:18 +0530

    mongodb added and fetching data from mongodb

--- client/main.js
+++ client/main.js
@@ -1,11 +1,17 @@
 import { Template } from 'meteor/templating';
+import { Notes } from '../lib/collection.js';
 
 import './main.html';
 
 Template.body.helpers({
+ /* 
   notes:[
     {text:'my note 1'},
     {text:'my note 2'},
     {text:'my note 3'}
   ]
+*/
+ notes(){
+   return Notes.find({});
+ }
 })
