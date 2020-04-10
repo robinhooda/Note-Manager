88fcae30bfbcafcca1ace61850a6aee9b68d4a54
Robin Hooda <robinhooda66@gmail.com>
2020-04-10 08:26:59 +0530

    note template added

--- client/main.js
+++ client/main.js
@@ -1,3 +1,11 @@
 import { Template } from 'meteor/templating';
 
 import './main.html';
+
+Template.body.helpers({
+  notes:[
+    {text:'my note 1'},
+    {text:'my note 2'},
+    {text:'my note 3'}
+  ]
+})
